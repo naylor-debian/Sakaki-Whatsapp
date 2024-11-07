737e8a0bf0898ad68886f117bd95b1814517c862
naylor-debian <95220392+naylor-debian@users.noreply.github.com>
2024-11-07 02:40:24 -0300

    Add files via upload

---
+++ Main/Public/Events/Messages.Upsert.ts
@@ -0,0 +1,13 @@
+module.exports = {
+ async function (sakaki: any ) {
+      sakaki.ev.on('messages.upsert', async upsert => {
+      const User = upsert.messages.[0].key.remoteJid
+    
+        console.log(JSON.stringify(upsert, undefined, 2))
+
+        console.log('replying to', User)
+        await sakaki.sendMessage(User, { text: 'Hello there!' })
+    }) 
+}
+
+}};
